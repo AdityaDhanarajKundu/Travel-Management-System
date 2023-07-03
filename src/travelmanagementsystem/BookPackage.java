cc9b9633a9d650b87a208aba0a75945a98047637
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2023-07-03 22:54:01 +0530

    creating the book package class

---
+++ src/travelmanagementsystem/BookPackage.java
@@ -0,0 +1,12 @@
+package travelmanagementsystem;
+
+import javax.swing.*;
+
+public class BookPackage extends JFrame {
+    BookPackage(){
+
+    }
+    public static void main(String[] args) {
+        new BookPackage();
+    }
+}
