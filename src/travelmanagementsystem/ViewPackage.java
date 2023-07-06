31e9b624f97708c1878d620133508d2e97db49e1
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2023-07-07 00:17:11 +0530

    creating the view book package class

---
+++ src/travelmanagementsystem/ViewPackage.java
@@ -0,0 +1,12 @@
+package travelmanagementsystem;
+
+import javax.swing.*;
+
+public class ViewPackage extends JFrame {
+    ViewPackage(){
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
