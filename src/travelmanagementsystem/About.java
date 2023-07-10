cb819f0ed912ea8de465a9d756610a7d03fd2bfd
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2023-07-11 00:11:50 +0530

    creating the about class

---
+++ src/travelmanagementsystem/About.java
@@ -0,0 +1,13 @@
+package travelmanagementsystem;
+
+import javax.swing.*;
+
+public class About extends JFrame {
+    About(){
+
+    }
+
+    public static void main(String[] args) {
+        new About();
+    }
+}
